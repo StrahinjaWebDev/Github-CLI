4525d299af11707ee0484ae7122943e4b7969b13
Strahinja <strahinjdevloper@gmail.com>
2025-03-30 16:38:59 +0200

    empty interfaces

--- game/game.go
+++ game/game.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var i1 Item
@@ -40,7 +42,41 @@ func main() {
 	for _, m := range ms {
 		fmt.Println(m)
 	}
+
+	k := Jade
+	fmt.Println("k:", k)
+
+	// time.Time import json.Marshal interface
+	// json.NewEncoder(os.Stdout).Encode(time.Now())
+
+	p1.FoundKey(Jade)
+	fmt.Println(p1.Keys)
+	p1.FoundKey(Jade)
+	fmt.Println(p1.Keys)
 }
+
+// Implement fmt.Stringer interface
+func (k Key) String() string {
+	switch k {
+	case Jade:
+		return "jade"
+	case Copper:
+		return "copper"
+	case Crystal:
+		return "crystal"
+	}
+
+	return fmt.Sprintf("<Key %d>", k)
+}
+
+const (
+	Jade Key = iota + 1
+	Copper
+	Crystal
+	invalidKey
+)
+
+type Key byte
 
 func moveAll(ms []mover, x, y int) {
 	for _, m := range ms {
@@ -53,9 +89,29 @@ type mover interface {
 	// Move(int,int)
 }
 
+func (p *Player) FoundKey(k Key) error {
+	if k < Jade || k >= invalidKey {
+		return fmt.Errorf("invalid key: %#v", k)
+	}
+	if !containsKey(p.Keys, k) {
+		p.Keys = append(p.Keys, k)
+	}
+	return nil
+}
+
+func containsKey(keys []Key, k Key) bool {
+	for _, k2 := range keys {
+		if k2 == k {
+			return true
+		}
+	}
+	return false
+}
+
 type Player struct {
 	Name string
 	Item // embeded item
+	Keys []Key
 }
 
 // i is called the reciver
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Golang_practise_next_lesson_7
+
+go 1.24
